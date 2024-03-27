be836af49125bb6907ffb1a7fc3f314e17013d3b
Axel <66967731+imxale@users.noreply.github.com>
2024-03-27 13:39:44 +0100

    feat(home): ajout home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,9 @@
 'use client'
 
 import { useGSAP } from "@gsap/react";
-import styles from "./page.module.css";
+import { useEffect } from "react";
+import styles from "./assets/styles/page.module.css";
+import reset from "./assets/styles/reset.css";
 import { gsap} from "gsap";
 import { useRef } from "react";
 import { ScrollTrigger } from "gsap/all";
@@ -10,34 +12,52 @@ gsap.registerPlugin(useGSAP);
 gsap.registerPlugin(ScrollTrigger);
 
 export default function Home() {
-  const circle = useRef();
-  const container = useRef();
-  useGSAP(() => {
-  gsap.to(".box", {x: 360});
-  }, { scope: container }); 
-
-  useGSAP(() => {
-    gsap.to(".box", {
-      scrollTrigger: {
-      trigger: '.box',
-      start: "30% center",
-      duration: 1,
-      markers: true,
-      toggleActions: 'play reverse play reverse',
-      },
-      x: "100%",
-      opacity: 0,
-      filter: 'blur(20px)'
-    })
-  });
+  // useGSAP(() => {
+  // gsap.to(".box", {x: 360});
+  // }, { scope: container });
+  //
+  // useGSAP(() => {
+  //   gsap.to(".box", {
+  //     scrollTrigger: {
+  //     trigger: '.box',
+  //     start: "30% center",
+  //     duration: 1,
+  //     scrub: true,
+  //     markers: true,
+  //     toggleActions: 'play reverse play reverse',
+  //     },
+  //     x: "100%",
+  //     opacity: 0,
+  //     filter: 'blur(20px)'
+  //   })
+  // });
 
   return (
-    <main className={styles.main}>
-      <h1>Hello, Next !</h1>
-      <div ref={container} className="">
-        <h1 className="text-5xl">HOME PAGE </h1>
-        <a href="/about" className="text-2xl">ABOUT </a>
-        <div ref={circle} className="box">YOLO </div>
+    <main className={styles.dot}>
+      <div className={styles.main}>
+        <div className={styles.home}>
+          <div className={styles.darkPurple}>
+            <p className={styles.whatIf}>
+              What if ?
+            </p>
+          </div>
+          <div className={styles.blue}>
+            <p className={styles.textTwitch}>
+              Que sont-ils devenus ?
+            </p>
+            {/* eslint-disable-next-line @next/next/no-img-element */}
+            <img className={styles.meme} src="https://clipart-library.com/img1/1326461.png" alt="Meme" />
+            <p className={styles.textSubtitle}>
+              {/* eslint-disable-next-line react/no-unescaped-entities */}
+              Utiliser les touches du clavier.
+            </p>
+          </div>
+          <div className={styles.purple}>
+            <img className={styles.logoTwitch}
+                 src={"https://upload.wikimedia.org/wikipedia/commons/thumb/8/80/Twitch_Glitch_Logo_Black.svg/878px-Twitch_Glitch_Logo_Black.svg.png"}
+            />
+          </div>
+        </div>
       </div>
     </main>
   );
