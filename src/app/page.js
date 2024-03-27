979b370ed3fb27827a78871e6a51c33c98917ede
k-guest <k.guest.fr@gmail.com>
2024-03-27 10:36:01 +0100

    First Profile Version

--- src/app/page.js
+++ src/app/page.js
@@ -6,6 +6,25 @@ import { gsap} from "gsap";
 import { useRef } from "react";
 import { ScrollTrigger } from "gsap/all";
 
+const GOTAGA = [
+  {
+    profile: {
+      pseudonyme: "Gotaga",
+      picture: "https://intrld.com/wp-content/uploads/2021/06/gotaga-twitch.png",
+      name: "Corentin Houssein",
+      age : "30"
+    },
+    timeline: {
+      box_one: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse id enim sit amet massa efficitur pretium quis sed odio.",
+      box_two: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse id enim sit amet massa efficitur pretium quis sed odio.",
+      box_three: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse id enim sit amet massa efficitur pretium quis sed odio.",
+      box_four: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse id enim sit amet massa efficitur pretium quis sed odio.",
+      box_five: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse id enim sit amet massa efficitur pretium quis sed odio.",
+      box_six: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse id enim sit amet massa efficitur pretium quis sed odio."
+    }
+  }
+]
+
 gsap.registerPlugin(useGSAP);
 gsap.registerPlugin(ScrollTrigger);
 
@@ -32,6 +51,7 @@ export default function Home() {
   });
 
   return (
+    <>
     <main className={styles.main}>
       <h1>Hello, Next !</h1>
       <div ref={container} className="">
@@ -40,5 +60,32 @@ export default function Home() {
         <div ref={circle} className="box">YOLO </div>
       </div>
     </main>
+    {GOTAGA.map( ( { ...props }, index ) => (
+      <div key={index} className={styles.profile}>
+        <div className={styles.profile_info}>
+          <p className={styles.profile_title}>What If</p>
+          <div className={styles.profile_card}>
+            <p className={styles.profile_pseudonyme}>{props.profile.pseudonyme}</p>
+            <img className={styles.profile_picture} src={props.profile.picture} alt="Picture" />
+          </div>
+          <div className={styles.profile_information}>
+            <div>
+              <p className={styles.profile_name}>{props.profile.name}</p>
+              <p className={styles.profile_age}>{props.profile.age} ans</p>
+            </div>
+          </div>
+        </div>
+        <div key={index} className={styles.profile_timeline}>
+          <p className={styles.box_one}>{props.timeline.box_one}</p>
+          <p className={styles.box_two}>{props.timeline.box_two}</p>
+          <p className={styles.box_three}>{props.timeline.box_three}</p>
+          <p className={styles.box_four}>{props.timeline.box_four}</p>
+          <p className={styles.box_five}>{props.timeline.box_five}</p>
+          <p className={styles.box_six}>{props.timeline.box_six}</p>
+        </div>
+        <span className={styles.row}>Flèche</span>
+      </div>
+    ) )}
+    </>
   );
 }
