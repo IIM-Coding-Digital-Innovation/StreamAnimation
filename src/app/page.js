bff5b76dc322fd28337bfc1e2e1667fb7e5e1cd1
k-guest <k.guest.fr@gmail.com>
2024-03-27 15:55:29 +0100

    Second Profile Version

--- src/app/page.js
+++ src/app/page.js
@@ -6,27 +6,96 @@ import { gsap} from "gsap";
 import { useRef } from "react";
 import { ScrollTrigger } from "gsap/all";
 
+gsap.registerPlugin(useGSAP);
+gsap.registerPlugin(ScrollTrigger);
+
 const GOTAGA = [
   {
     profile: {
       pseudonyme: "Gotaga",
       picture: "https://intrld.com/wp-content/uploads/2021/06/gotaga-twitch.png",
       name: "Corentin Houssein",
-      age : "30"
+      age : "30",
+      color: "#C51A26"
     },
-    timeline: {
-      box_one: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse id enim sit amet massa efficitur pretium quis sed odio.",
-      box_two: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse id enim sit amet massa efficitur pretium quis sed odio.",
-      box_three: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse id enim sit amet massa efficitur pretium quis sed odio.",
-      box_four: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse id enim sit amet massa efficitur pretium quis sed odio.",
-      box_five: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse id enim sit amet massa efficitur pretium quis sed odio.",
-      box_six: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse id enim sit amet massa efficitur pretium quis sed odio."
-    }
+    timeline: [
+      {
+        box_number: "box_one",
+        year: "2025",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse id enim sit amet massa efficitur pretium quis sed odio."
+      },
+      {
+        box_number: "box_two",
+        year: "2025",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse id enim sit amet massa efficitur pretium quis sed odio."
+      },
+      {
+        box_number: "box_three",
+        year: "2025",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse id enim sit amet massa efficitur pretium quis sed odio."
+      },
+      {
+        box_number: "box_four",
+        year: "2025",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse id enim sit amet massa efficitur pretium quis sed odio."
+      },
+      {
+        box_number: "box_five",
+        year: "2025",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse id enim sit amet massa efficitur pretium quis sed odio."
+      },
+      {
+        box_number: "box_six",
+        year: "2025",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse id enim sit amet massa efficitur pretium quis sed odio."
+      }
+    ]
   }
 ]
 
-gsap.registerPlugin(useGSAP);
-gsap.registerPlugin(ScrollTrigger);
+const KAMETO = [
+  {
+    profile: {
+      pseudonyme: "Kameto",
+      picture: "https://img.redbull.com/images/c_limit,w_1500,h_1000,f_auto,q_auto/redbullcom/2023/3/12/cn2fwwvzlzu2d8c960sp/kameto-commentateur-esport",
+      name: "Kamel Kebir",
+      age : "28",
+      color: "#004687"
+    },
+    timeline: [
+      {
+        box_number: "box_one",
+        year: "2025",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse id enim sit amet massa efficitur pretium quis sed odio."
+      },
+      {
+        box_number: "box_two",
+        year: "2025",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse id enim sit amet massa efficitur pretium quis sed odio."
+      },
+      {
+        box_number: "box_three",
+        year: "2025",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse id enim sit amet massa efficitur pretium quis sed odio."
+      },
+      {
+        box_number: "box_four",
+        year: "2025",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse id enim sit amet massa efficitur pretium quis sed odio."
+      },
+      {
+        box_number: "box_five",
+        year: "2025",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse id enim sit amet massa efficitur pretium quis sed odio."
+      },
+      {
+        box_number: "box_six",
+        year: "2025",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse id enim sit amet massa efficitur pretium quis sed odio."
+      }
+    ]
+  }
+]
 
 export default function Home() {
   const circle = useRef();
@@ -41,7 +110,6 @@ export default function Home() {
       trigger: '.box',
       start: "30% center",
       duration: 1,
-      markers: true,
       toggleActions: 'play reverse play reverse',
       },
       x: "100%",
@@ -50,42 +118,60 @@ export default function Home() {
     })
   });
 
+  useGSAP(() => {
+    gsap.to("#children_box p", {
+      y: -3050,
+      scrollTrigger: {
+          trigger: "#parent_box",
+          start: "top 75",
+          end: "+=1000",
+          scrub: true,
+          markers: true,
+          pin: true,
+      }
+    });
+
+    return () => {
+        ScrollTrigger.getAll().forEach(trigger => trigger.kill());
+    };
+  }, []);
+
   return (
     <>
-    <main className={styles.main}>
-      <h1>Hello, Next !</h1>
-      <div ref={container} className="">
-        <h1 className="text-5xl">HOME PAGE </h1>
-        <a href="/about" className="text-2xl">ABOUT </a>
-        <div ref={circle} className="box">YOLO </div>
-      </div>
-    </main>
-    {GOTAGA.map( ( { ...props }, index ) => (
-      <div key={index} className={styles.profile}>
-        <div className={styles.profile_info}>
-          <p className={styles.profile_title}>What If</p>
-          <div className={styles.profile_card}>
-            <p className={styles.profile_pseudonyme}>{props.profile.pseudonyme}</p>
-            <img className={styles.profile_picture} src={props.profile.picture} alt="Picture" />
-          </div>
-          <div className={styles.profile_information}>
-            <div>
-              <p className={styles.profile_name}>{props.profile.name}</p>
-              <p className={styles.profile_age}>{props.profile.age} ans</p>
+      <main className={styles.main}>
+        <h1>Hello, Next !</h1>
+        <div ref={container} className="">
+          <h1 className="text-5xl">HOME PAGE </h1>
+          <a href="/about" className="text-2xl">ABOUT </a>
+          <div ref={circle} className="box">YOLO </div>
+        </div>
+      </main>
+      {GOTAGA.map( ( { ...props }, index ) => (
+        <div key={index} className={styles.profile} id="parent_box" style={{ height: "100vh" }}>
+          <div className={styles.profile_info} style={{backgroundColor: props.profile.color}}>
+            <p className={styles.profile_title}>What If</p>
+            <div className={styles.profile_card}>
+              <p className={styles.profile_pseudonyme}>{props.profile.pseudonyme}</p>
+              <img className={styles.profile_picture} src={props.profile.picture} alt="Picture" />
+            </div>
+            <div className={styles.profile_information}>
+              <div>
+                <p className={styles.profile_name}>{props.profile.name}</p>
+                <p className={styles.profile_age}>{props.profile.age} ans</p>
+              </div>
             </div>
           </div>
+          <div className={styles.profile_timeline} id="children_box" style={{ height: "80vh" }}>
+          {props.timeline.map( ( { ...data }, index ) => (
+            <div key={index} className={styles[data.box_number]}>
+              <p className={styles.box_year}>{data.year}</p>
+              <p className={styles.box_text}>{data.text}</p>
+            </div>
+          ) )}
+          </div>
+          <span className={styles.row} style={{backgroundColor: props.profile.color}} />
         </div>
-        <div key={index} className={styles.profile_timeline}>
-          <p className={styles.box_one}>{props.timeline.box_one}</p>
-          <p className={styles.box_two}>{props.timeline.box_two}</p>
-          <p className={styles.box_three}>{props.timeline.box_three}</p>
-          <p className={styles.box_four}>{props.timeline.box_four}</p>
-          <p className={styles.box_five}>{props.timeline.box_five}</p>
-          <p className={styles.box_six}>{props.timeline.box_six}</p>
-        </div>
-        <span className={styles.row}>Flèche</span>
-      </div>
-    ) )}
+      ) )}
     </>
   );
 }
